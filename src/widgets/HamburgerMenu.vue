4ec68e0516d0caef8edadba27a5e706bb2d2b279
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-02 11:13:43 +0200

    001 add hamburger component

---
+++ src/widgets/HamburgerMenu.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="hamburger-menu" @click="$emit('openMenu')">
+    <div class="hamburger"></div>
+    <div class="hamburger"></div>
+    <div class="hamburger"></div>
+    <div class="hamburger"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.hamburger-menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-self: center;
+  width: 30px;
+  height: 20px;
+  cursor: pointer;
+}
+
+.hamburger {
+  height: 3px;
+  width: 100%;
+  background-color: var(--primary-color);
+  border-radius: 5px;
+}
+</style>
