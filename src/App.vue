6ffe31d3a0e9b1e957f5bb810f9ae7561f5540c2
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-08 11:49:40 +0200

    001 add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HeaderComponent from './components/HeaderComponent.vue'
+import Footer from './components/FooterComponent.vue'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <HeaderComponent />
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped></style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <span class="footer__title"> Zuzana Volfová</span>
+    <span> +420 728 881 941</span>
+    <a class="footer__link" href="mailto:zuzka.volfova@gmail.com">zuzka.volfova@gmail.com</a>
+    <a class="footer__link" href="www.linkedin.com/in/zuzana-volfová-90001530">LinkedIn</a>
+  </div>
+</template>
+<style lang="scss" scoped>
+.footer {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-items: center;
+  width: 100%;
+  color: var(--text-dark-grey);
+  padding: 12px 0;
+  border-top: 1px solid var(--light-background);
+  &__title {
+    font-weight: 600;
+  }
+  &__link {
+    color: var(--text-dark-grey);
+    text-decoration: none;
+  }
+}
+</style>
