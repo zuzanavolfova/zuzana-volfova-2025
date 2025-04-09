d40127f16c272e2543332564cd46d3e528a02da1
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-09 10:37:44 +0200

    001 add card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+  cardTitle: {
+    type: String,
+    required: false,
+  },
+  cardImage: {
+    type: String,
+    required: false,
+  },
+  cardDescriptionShort: {
+    type: String,
+    required: false,
+  },
+  cardDescription: {
+    type: String,
+    required: false,
+  },
+  buttonTitle: {
+    type: String,
+    required: false,
+    default: 'GitHub repository',
+  },
+})
+</script>
+<template>
+  <div class="card__container">
+    <div class="card__item">
+      <h4 class="card__item__heading">{{ cardTitle }}</h4>
+      <p class="card__item__image">
+        <a href="#"
+          ><img :src="cardImage" :alt="cardDescriptionShort" width="300" height="200"
+        /></a>
+      </p>
+      <p class="card__item__perex">{{ cardDescription }}</p>
+      <p>
+        <button>{{ buttonTitle }}</button>
+      </p>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.card {
+  &__container {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(250px, calc(300px + 4em)));
+    grid-template-rows: repeat(4, auto);
+    column-gap: 4rem;
+    padding: 1em;
+    margin: 2em;
+    max-width: 1000px;
+  }
+  &__item {
+    display: grid;
+    grid-template-rows: subgrid;
+    grid-row: span 4;
+  }
+}
+
+img {
+  height: auto;
+  max-width: 100%;
+}
+</style>
--- src/views/CoddingView.vue
+++ src/views/CoddingView.vue
@@ -1,8 +1,27 @@
+<script setup lang="ts">
+import CardComponent from './../components/CardComponent.vue'
+</script>
+
 <template>
   <div class="codding">
     <h2 class="codding__title">{{ $t('coding-h') }}</h2>
-
-    <div class="codding__content"></div>
+    <div class="codding__content">
+      <div class="codding__content__item">
+        <h3>Vue.js, React</h3>
+        <card-component
+          card-title="Zuzana Volfova websites"
+          card-image="./../assets/images/image.png"
+          card-description-short="redirect to GitHub"
+          card-description="Vue.js, Pinia, Typescript, Bootstrap, HitHub"
+        />
+      </div>
+      <div>
+        <h3>HTML, CSS</h3>
+      </div>
+      <div>
+        <h3>JavaScript</h3>
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -13,10 +32,12 @@
   }
   &__content {
     display: flex;
-    justify-content: center;
-    width: 100%;
-    &__title {
-      text-align: center;
+    flex-direction: column;
+    text-align: center;
+    align-content: space-around;
+    &__item {
+      display: grid;
+      grid-template-rows: 1fr 2fr;
     }
   }
 }
