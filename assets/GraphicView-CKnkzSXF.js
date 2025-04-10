8f261ae3523af4b221e7a8002652513eae38b64f
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-10 13:40:31 +0200

    Updates

---
+++ assets/GraphicView-CKnkzSXF.js
@@ -0,0 +1,1 @@
+import{b as e,c,a as t,t as o,o as r}from"./index-D7wq46m4.js";const i={},n={class:"graphic"},_={class:"graphic__title"};function l(a,s){return r(),c("div",n,[t("h2",_,o(a.$t("graphic-h")),1),s[0]||(s[0]=t("div",{class:"graphic__content"},null,-1))])}const d=e(i,[["render",l],["__scopeId","data-v-b9595bb8"]]);export{d as default};
