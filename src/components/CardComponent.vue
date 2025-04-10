bf118489b0c8331cf813349160790ada5f7102ff
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-10 13:23:23 +0200

    001 style card componet, add functionaity to get images

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { defineProps, ref, watchEffect } from 'vue'
 
-defineProps({
+const props = defineProps({
   cardTitle: {
     type: String,
     required: false,
@@ -23,44 +23,68 @@ defineProps({
     required: false,
     default: 'GitHub repository',
   },
+  buttonRedirect: {
+    type: String,
+    required: false,
+  },
+})
+const imageUrl = ref('')
+
+watchEffect(async () => {
+  if (!props.cardImage) {
+    imageUrl.value = ''
+    return
+  }
+
+  try {
+    const imageModule = await import(`../assets/images/${props.cardImage}`)
+    imageUrl.value = imageModule.default
+  } catch (error) {
+    console.error('Failed to load image:', error)
+    imageUrl.value = ''
+  }
 })
 </script>
 <template>
   <div class="card__container">
     <div class="card__item">
-      <h4 class="card__item__heading">{{ cardTitle }}</h4>
+      <h4 class="card__item__title">{{ cardTitle }}</h4>
       <p class="card__item__image">
-        <a href="#"
-          ><img :src="cardImage" :alt="cardDescriptionShort" width="300" height="200"
-        /></a>
+        <img :src="imageUrl" :alt="cardImage" height="80px" />
       </p>
+
       <p class="card__item__perex">{{ cardDescription }}</p>
       <p>
-        <button>{{ buttonTitle }}</button>
+        <button class="card__item__button">{{ buttonTitle }}</button>
       </p>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
+@import '@/assets/styles/mixins';
 .card {
   &__container {
-    display: grid;
-    grid-template-columns: repeat(2, minmax(250px, calc(300px + 4em)));
-    grid-template-rows: repeat(4, auto);
-    column-gap: 4rem;
-    padding: 1em;
-    margin: 2em;
-    max-width: 1000px;
+    margin: 28px;
+    height: 380px;
   }
   &__item {
-    display: grid;
-    grid-template-rows: subgrid;
-    grid-row: span 4;
+    box-shadow: 1px 1px 5px rgb(223, 223, 223);
+    padding: 1rem;
+    border-radius: 2px;
+    &__title {
+      height: 56px;
+      @media (min-width: 1250px) {
+        height: 28px;
+      }
+    }
+    &__button {
+      @include button;
+    }
   }
 }
 
 img {
-  height: auto;
+  height: 160px;
   max-width: 100%;
 }
 </style>
