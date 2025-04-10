5011c595e42bca5a5aae29db04de094804bd6b76
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-10 14:42:36 +0200

    001 added temp link to old pages

--- src/views/GraphicView.vue
+++ src/views/GraphicView.vue
@@ -2,7 +2,16 @@
   <div class="graphic">
     <h2 class="graphic__title">{{ $t('graphic-h') }}</h2>
 
-    <div class="graphic__content"></div>
+    <div class="graphic__content">
+      <span>In progress. Please see old pages:</span>
+      <a
+        href="https://zuzana-volfova.netlify.app/grafika"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Zuzana Volfová – Grafika
+      </a>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -13,8 +22,10 @@
   }
   &__content {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     width: 100%;
+    text-align: center;
     &__title {
       text-align: center;
     }
