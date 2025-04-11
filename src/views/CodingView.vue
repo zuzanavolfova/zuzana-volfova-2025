93d3ef509ca4a38fd67c0b91011de94004ecfdb2
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-11 11:44:09 +0200

    001 add hover effect to card

--- src/views/CodingView.vue
+++ src/views/CodingView.vue
@@ -71,6 +71,10 @@ const javaScriptData = dataJson.javaScript
       }
     }
     &__item {
+      &:hover {
+        transform: scale(1.06);
+        transition: transform 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+      }
       @media (min-width: 650px) {
         flex-basis: 50%;
       }
