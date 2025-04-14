b073be754bd495a72c4d6d217af3811c28b68f4b
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-14 11:22:25 +0200

    001 add ux design content

--- src/views/UXDesignView.vue
+++ src/views/UXDesignView.vue
@@ -1,16 +1,24 @@
+<script setup lang="ts">
+import CardComponent from '../components/CardComponent.vue'
+import uxData from './../assets/data/ux-data.json'
+</script>
+
 <template>
   <div class="ux">
     <h2 class="ux__title">{{ $t('ux-design-h') }}</h2>
 
     <div class="ux__content">
-      <span>In progress. Please see old pages:</span>
-      <a
-        href="https://zuzana-volfova.netlify.app/ux-design/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Zuzana Volfová – UX design
-      </a>
+      <div class="ux__content__item" v-for="(item, index) in uxData" :key="index">
+        <card-component
+          :card-title="item.cardTitle"
+          :card-image="item.cardImage"
+          :card-description-short="item.cardDescriptionShort"
+          :card-description="item.cardDescription"
+          :card-descriptionParams="item.translationParams"
+          :button-title="item.buttonTitle"
+          :button-redirect="item.buttonRedirect"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -23,11 +31,24 @@
   &__content {
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    width: 100%;
     text-align: center;
-    &__title {
-      text-align: center;
+    &__items {
+      @media (min-width: 650px) {
+        display: flex;
+        flex-wrap: wrap;
+      }
+    }
+    &__item {
+      &:hover {
+        transform: scale(1.06);
+        transition: transform 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+      }
+      @media (min-width: 650px) {
+        flex-basis: 50%;
+      }
+      @media (min-width: 950px) {
+        flex-basis: 33%;
+      }
     }
   }
 }
