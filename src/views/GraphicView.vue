e5b9443a0c8bdc3b04b17cae672160cd6a3e0352
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-17 11:09:25 +0200

    001 ad graphicvie content

--- src/views/GraphicView.vue
+++ src/views/GraphicView.vue
@@ -1,13 +1,41 @@
+<script setup lang="ts">
+import imageData from './../assets/data/img-data.json'
+const imagePromotionalData = imageData.promotional
+const imageVectorData = imageData.vector
+const getImageUrl = (source: string) => {
+  return new URL(`../assets/images/graphic`, import.meta.url).href + '/' + source
+}
+</script>
 <template>
   <div class="graphic">
     <h2 class="graphic__title">{{ $t('graphic-h') }}</h2>
 
     <div class="graphic__content">
       <section>
-        <h2>{{ $t('promotional-graphics') }}</h2>
+        <h3>{{ $t('promotional-graphics') }}</h3>
+        <div class="graphic__content__items">
+          <div v-for="(image, key) in imagePromotionalData" :key="key">
+            <img
+              :src="getImageUrl(image.imageSource)"
+              :alt="image.imageDescriptionShort"
+              height="220px"
+              loading="lazy"
+            />
+          </div>
+        </div>
       </section>
       <section>
-        <h2>{{ $t('vector-graphics') }}</h2>
+        <h3>{{ $t('vector-graphics') }}</h3>
+        <div class="graphic__content__items">
+          <div v-for="(image, key) in imageVectorData" :key="key">
+            <img
+              :src="getImageUrl(image.imageSource)"
+              :alt="image.imageDescriptionShort"
+              height="220px"
+              loading="lazy"
+            />
+          </div>
+        </div>
       </section>
     </div>
   </div>
@@ -31,6 +59,18 @@
     text-align: center;
     &__title {
       text-align: center;
+    }
+    &__items {
+      display: flex;
+      flex-direction: column;
+      gap: 28px;
+      @media (min-width: 650px) {
+        flex-direction: row;
+        gap: 42px;
+        flex-wrap: wrap;
+        justify-content: center;
+        padding: 24px;
+      }
     }
   }
 }
