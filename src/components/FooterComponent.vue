662c2d179e45cfd5e109de655f95e9bd8ee3d3fb
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-05-15 09:10:52 +0200

    add accesibility to footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="footer">
-    <span class="footer__title"> Zuzana Volfová</span>
-    <span> +420 728 881 941</span>
-    <a class="footer__link" href="mailto:zuzka.volfova@gmail.com">zuzka.volfova@gmail.com</a>
-    <a class="footer__link" href="https://www.linkedin.com/in/zuzana-volfová">LinkedIn</a>
-  </div>
+  <footer class="footer" role="contentinfo" aria-label="Contact information for Zuzana Volfová">
+    <span class="footer__title" aria-label="Name"> Zuzana Volfová</span>
+    <span aria-label="Phone number"> +420 728 881 941</span>
+    <a
+      class="footer__link"
+      aria-label="Send an email to zuzka.volfova@gmail.com"
+      href="mailto:zuzka.volfova@gmail.com"
+      >zuzka.volfova@gmail.com</a
+    >
+    <a
+      class="footer__link"
+      role="link"
+      aria-label="Visit Zuzana Volfová's LinkedIn profile"
+      href="https://www.linkedin.com/in/zuzana-volfová"
+      >LinkedIn</a
+    >
+  </footer>
 </template>
 <style lang="scss" scoped>
 .footer {
