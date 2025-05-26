bf512fe6423816f46dab034e17028c6ca2662261
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-05-26 09:45:33 +0200

    003 add getData functionality

---
+++ src/stores/getters.ts
@@ -0,0 +1,20 @@
+import type { CardData } from '../types/types'
+
+export const getData = async (): Promise<CardData[]> => {
+  try {
+    const response: Response = await fetch(
+      'https://raw.githubusercontent.com/zuzanavolfova/data/refs/heads/main/coding-data.json',
+      { method: 'GET' },
+    )
+
+    if (!response.ok) {
+      throw new Error(`Data could not be loaded: ${response.status}`)
+    }
+
+    const data: CardData[] = await response.json()
+    return data
+  } catch (error: unknown) {
+    console.error('Error loading json:', error)
+    throw error
+  }
+}
