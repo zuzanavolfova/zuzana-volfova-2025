a84c80438e025e6e65b6f4dfe5ac0eb2d67978b3
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-07-23 09:57:29 +0200

    Edit home view - add techstack

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,17 @@ import CarouselComponent from './../components/CarouselComponent.vue'
 
 <template>
   <main class="home">
+    <div class="home__text">
+      <h2>Frontend developer</h2>
+      <div class="home__techstack">
+        <p>Vue.js, React, JavaScript, TypeScript, Tailwind, Python</p>
+        <p>HTML, CSS, BEM, SASS</p>
+        <p>GitHub, Jira, Confluence, Azure, Sourcetree</p>
+        <p>Claude AI, ChatGPT, GitHub Copilot, Vibe coding</p>
+
+        <p>Figma, AdobeXD</p>
+      </div>
+    </div>
     <CarouselComponent />
   </main>
 </template>
@@ -13,6 +24,36 @@ import CarouselComponent from './../components/CarouselComponent.vue'
   padding: 10px;
   @media (min-width: 650px) {
     padding: 20px;
+  }
+  &__text {
+    margin: 30px auto 40px auto;
+    & h2 {
+      position: relative;
+      margin-bottom: 20px;
+      text-align: center;
+      font-weight: 400;
+      width: auto;
+      &::after {
+        content: '';
+        position: absolute;
+        bottom: -10px;
+        left: 0;
+        width: 100%;
+        height: 2px;
+        background: linear-gradient(
+          to right,
+          transparent 10%,
+          var(--primary-color) 30%,
+          var(--primary-color) 70%,
+          transparent 90%,
+          transparent 100%
+        );
+      }
+    }
+  }
+  &__techstack p {
+    font-size: 18px;
+    text-align: center;
   }
 }
 </style>
